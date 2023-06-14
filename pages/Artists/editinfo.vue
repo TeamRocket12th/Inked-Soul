993d7cd86ce43d2d77010d90fa5ea9bcff556b68
benson <benson95217@gmail.com>
2023-06-14 14:03:41 +0800

    feat: edit info and open time

---
+++ pages/Artists/editinfo.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <EditSelfArea />
+    <button type="button" class="btn my-5 w-full">更新個人資料</button>
+  </div>
+</template>
+<script setup>
+import EditSelfArea from '~/container/artists/EditSelfArea.vue'
+</script>
+<style scoped></style>
