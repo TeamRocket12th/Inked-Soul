b5d87c74a31869ade84459a3ed10125056c63653
NancyHsu1994Taiwan <jjuice258tw@gmail.com>
2023-06-28 17:33:09 +0800

    style: fix banner

--- components/home/BannerItem.vue
+++ components/home/BannerItem.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="relative flex h-screen items-center justify-center bg-[url(/home-banner.svg)]">
+  <div
+    class="relative flex h-screen items-center justify-center bg-[url(/home-banner.svg)] bg-cover"
+  >
     <!-- <img src="/home-banner.svg" alt="" class="absolute left-0 right-0 top-0 -z-10" /> -->
     <h2 class="text-white">探索專屬刺青故事</h2>
   </div>
