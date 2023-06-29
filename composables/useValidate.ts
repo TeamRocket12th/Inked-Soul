620687dfbdab06765491191d522bb219f47f6150
benson <benson95217@gmail.com>
2023-06-29 11:08:48 +0800

    feat: add artist backend style

--- composables/useValidate.ts
+++ composables/useValidate.ts
@@ -21,5 +21,25 @@ export const useValidate = () => {
     return true
   }
 
-  return { isPassword, isPhone }
+  const isUnder20 = (value: string): string | true => {
+    if (value) {
+      const characters = value.split('')
+      if (characters.length >= 20) {
+        return '僅能填寫 20 個字內'
+      }
+    }
+    return true
+  }
+
+  const isTel = (value: string): string | true => {
+    const regex = /^\d{2}-\d{8}$/
+    if (value) {
+      if (!regex.test(String(value))) {
+        return '電話格式不正確'
+      }
+    }
+    return true
+  }
+
+  return { isPassword, isPhone, isUnder20, isTel }
 }
