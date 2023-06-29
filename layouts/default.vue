4d0f653f151ac45f1bcac8b6f489f9b6deb22eee
NancyHsu1994Taiwan <67849065+NancyHsu1994Taiwan@users.noreply.github.com>
2023-06-29 10:41:04 +0800

    Update default.vue
    恢復import global元件

--- layouts/default.vue
+++ layouts/default.vue
@@ -16,7 +16,7 @@
 </template>
 
 <script setup>
-// import NavbarItem from '~/components/global/NavbarItem.vue'
+import NavbarItem from '~/components/global/NavbarItem.vue'
 // import FooterItem from '~/components/global/FooterItem.vue'
 const route = useRoute()
 </script>
