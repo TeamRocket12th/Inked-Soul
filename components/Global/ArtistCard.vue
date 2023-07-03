2ef0e75aa650b099fb3527f1fd959bf3cf5fb50b
NancyHsu1994Taiwan <jjuice258tw@gmail.com>
2023-07-03 16:43:03 +0800

    fix: fix layout detail

--- components/Global/ArtistCard.vue
+++ components/Global/ArtistCard.vue
@@ -1,8 +1,10 @@
 <template>
-  <div class="relative overflow-hidden rounded-lg">
+  <div class="group relative overflow-hidden rounded-lg">
     <img :src="`${props.image}`" alt="" class="w-[416px h-[416px] object-cover" />
-    <div class="absolute bottom-0 left-0 right-0 bg-black bg-opacity-50 p-4 text-white">
-      <div class="flex flex-col items-start gap-3">
+    <div
+      class="absolute bottom-0 left-0 right-0 hidden bg-black bg-opacity-50 p-4 text-white duration-300 group-hover:block"
+    >
+      <div class="flex flex-col items-start items-center gap-3 hover:flex">
         <p>{{ props.nickname }}</p>
         <span class="rounded-full border px-3 py-1">寫實主義 Realism</span>
       </div>
