cf919176cf2fbffe2bd7f7e871efeb05b96984d1
benson <benson95217@gmail.com>
2023-07-12 18:49:43 +0800

    feat: 接API，完成 order page

--- components/design/DesignImage.vue
+++ components/design/DesignImage.vue
@@ -1,14 +1,10 @@
 <template>
   <div class="flex w-full flex-col items-start gap-3 rounded-lg bg-white p-5 shadow">
-    <img
-      :src="`${props.designData.image}`"
-      alt=""
-      class="w-full rounded-lg border object-contain"
-    />
+    <img :src="`${props.designData.Url}`" alt="" class="w-full rounded-lg border object-contain" />
     <div class="flex w-full flex-col gap-5">
       <div class="flex w-full flex-col items-start gap-2">
         <div class="flex w-full flex-row items-center justify-between">
-          <h4>{{ props.designData.designName }}</h4>
+          <h4>{{ props.designData.Name }}</h4>
           <div>
             <div class="dropdown-end dropdown">
               <label
@@ -16,13 +12,13 @@
                 class="btn-outline btn border-none bg-white hover:bg-white hover:text-black"
                 ><Icon name="ic:outline-share" size="24"
               /></label>
-              <ul tabindex="0" class="dropdown-content z-[1] w-52 bg-base-100">
+              <ul tabindex="0" class="dropdown-content z-[1] w-52 rounded-lg bg-base-100">
                 <ShareModal />
               </ul>
             </div>
           </div>
         </div>
-        <p class="text-secondary">{{ props.designData.description }}</p>
+        <p class="text-secondary">{{ props.designData.Idea }}</p>
       </div>
       <ul class="flex flex-row items-center gap-2">
         <li v-for="(style, key) in props.designData.Style" class="rounded-full border px-3 py-1">
@@ -30,9 +26,6 @@
         </li>
       </ul>
     </div>
-
-    <!-- <div>風格：{{ props.designData.Style }}</div> -->
-    <!-- <div>元素：{{ props.designData.element }}</div> -->
   </div>
 </template>
 <script setup>
