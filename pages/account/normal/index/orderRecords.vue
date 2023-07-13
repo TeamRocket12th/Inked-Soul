91e08dd8df93ada8cbc54c576e6b0bc0e36d1e22
NancyHsu1994Taiwan <jjuice258tw@gmail.com>
2023-07-13 10:31:49 +0800

    feat: 取得所有訂單資料(進行中)

--- pages/account/normal/index/orderRecords.vue
+++ pages/account/normal/index/orderRecords.vue
@@ -15,30 +15,53 @@
           </tr>
         </thead>
         <tbody>
-          <OrderBar
+          <!-- <OrderBar
             v-for="item in data"
             :key="item.Id"
             :order="item"
             :status="item.Status"
             class="hover:cursor-pointer hover:bg-gray-300"
             @click="jump(item.Id)"
-          />
+          /> -->
         </tbody>
       </table>
+    </div>
+    <!-- 分頁 -->
+    <div class="join">
+      <button class="join-item join-item btn btn"></button>
+      <!-- @click="searchAllOrder('user',`${}`)" -->
     </div>
   </div>
 </template>
 
 <script setup>
+import { storeToRefs } from 'pinia'
 import { useOrderStore } from '~/stores/order'
+import OrderBar from '~/components/order/OrderBar'
 const store = useOrderStore()
 const { getAllOrder } = store
-import OrderBar from '~/components/order/OrderBar'
+const { AllOrderRecord } = storeToRefs(store)
+const authToken = useCookie('token')
+const runtimeConfig = useRuntimeConfig()
+const APIBASE = runtimeConfig.public.APIBASE
 
 // 資料庫建置完成後改用store.js的getAllOrder
-const data = await $fetch('/api/getOrder/getAllOrder')
-
-console.log('data:', data)
+// const data = await $fetch('/api/getOrder/getAllOrder')
+const { data } = await useFetch(`${APIBASE}/api/getimage/imgorder`, {
+  method: 'POST',
+  headers: {
+    'Content-type': 'application/json',
+    Authorization: `Bearer ${authToken.value}`
+  },
+  body: {
+    role: 'user'
+  },
+  query: {
+    pageNumber: 1,
+    pageSize: 10
+  }
+})
+console.log('取得訂單資料:', data)
 // 有真資料後再使用已下這段
 // const order = ref([])
 // order.value = data.value.OrderData
@@ -49,7 +72,7 @@ const jump = (input) => {
   navigateTo(`/account/normal/${input}`)
 }
 
-// onMounted(()=>{
-//   getAllOrder()
-// })
+// 分頁
+const pageNum = ref()
+pageNum.value = Math.ceil(AllOrderRecord.value.length / 10)
 </script>
