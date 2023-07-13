c0809a5d957a5f7fa9352739225bb613864a42dd
NancyHsu1994Taiwan <jjuice258tw@gmail.com>
2023-07-14 07:39:06 +0800

    fix: 重構函式(進行中)

--- container/searchAllArtists/ArtistsArea.vue
+++ container/searchAllArtists/ArtistsArea.vue
@@ -13,13 +13,13 @@
       </div>
     </div>
     <div class="grid w-full grid-cols-3 gap-8">
-      <NuxtLink v-for="item in allArtistsData" :key="item.id" :to="`/artists/${item.Id}`">
+      <!-- <NuxtLink v-for="item in allArtistsData" :key="item.id" :to="`/artists/${item.Id}`">
         <ArtistCard
           :image="item.Photo"
           :nickname="item.Nickname"
           :category="item.style"
         ></ArtistCard>
-      </NuxtLink>
+      </NuxtLink> -->
     </div>
   </div>
 </template>
@@ -28,16 +28,20 @@ import { storeToRefs } from 'pinia'
 import { useSearchStore } from '~/stores/search'
 const store = useSearchStore()
 const { allArtistsData, cityArr, showResult } = storeToRefs(store)
+const { getArtists } = store
 
-const runtimeConfig = useRuntimeConfig()
-const APIBASE = runtimeConfig.public.APIBASE
-const { data } = await useFetch(`${APIBASE}/api/searchartist`, {
-  method: 'POST',
-  body: {
-    City: '',
-    Style: '',
-    Element: ''
-  }
+// const runtimeConfig = useRuntimeConfig()
+// const APIBASE = runtimeConfig.public.APIBASE
+// const { data } = await useFetch(`${APIBASE}/api/searchartist`, {
+//   method: 'POST',
+//   body: {
+//     City: '',
+//     Style: '',
+//     Element: ''
+//   }
+// })
+// allArtistsData.value = data.value.Data
+onMounted(() => {
+  getArtists()
 })
-allArtistsData.value = data.value.Data
 </script>
