5ee72c36d617fde8d8c65d522ffed0a8e34d4dc4
benson <benson95217@gmail.com>
2023-07-18 14:06:05 +0800

    style: 修改顯示＆樣式

--- components/Global/DesignCard.vue
+++ components/Global/DesignCard.vue
@@ -1,53 +1,70 @@
 <template>
   <div class="boreder-black group relative overflow-hidden rounded-lg border-2">
-    <img :src="props.image" alt="" class="h-[200px]" :style="{ height: `${height}px` }" />
+    <img
+      :src="props.image"
+      class="h-fit min-h-[200px] w-full bg-white object-contain object-center"
+    />
+    <!-- :style="{ height: `${height}px` }" -->
     <!-- 遮罩 -->
     <div
-      class="z-1 absolute top-0 hidden h-full w-full flex-col-reverse bg-gradient-to-b from-transparent to-black p-2 group-hover:flex"
+      class="z-1 designCard absolute top-0 hidden h-full w-full flex-col-reverse gap-3 p-5 group-hover:flex"
     >
-      <div class="flex justify-between">
-        <p class="text-white">{{ props.designName }}</p>
-        <p class="text-white">${{ props.price }}</p>
+      <div class="flex items-center justify-between">
+        <p class="font-noto-tc font-bold text-white">{{ props.designName }}</p>
+        <p class="font-roboto-nl font-bold text-white">$ {{ props.price }}</p>
       </div>
-      <div class="flex items-center">
-        <img :src="props.artistImg" alt="" class="mr-2 h-[40px] w-[40px] rounded-full" />
-        <p class="text-white">{{ props.artistName }}</p>
+      <div class="flex items-center" :class="{ hidden: isArtistPage }">
+        <img
+          :src="props.artistImg"
+          alt=""
+          class="mr-2 h-[40px] w-[40px] rounded-lg object-cover object-center"
+        />
+        <p class="font-roboto-nl font-bold text-white">{{ props.artistName }}</p>
       </div>
     </div>
   </div>
 </template>
 <script setup>
-const props = defineProps({
-  id: {
-    required: true
-    // type: Number
-  },
-  image: {
-    required: true
-    // type: String
-  },
-  designName: {
-    required: true
-    // type: String
-  },
-  artistName: {
-    required: true,
-    // type: String,
-    default: 'dafeult artist name'
-  },
-  artistImg: {
-    required: true
-    // type: String
-  },
-  price: {
-    required: true
-    // type: Number
+const route = useRoute()
+const isArtistPage = computed(() => {
+  if (route.params.artistID) {
+    return true
+  } else {
+    return false
   }
 })
 
-// 測試用
-const random = ref(Math.floor(Math.random() * 150))
-const height = computed(() => {
-  return 200 + random.value
+const props = defineProps({
+  id: {
+    type: null, // Number
+    required: true
+  },
+  image: {
+    type: null, // String
+    required: true
+  },
+  designName: {
+    type: null, // String
+    required: true
+  },
+  artistName: {
+    type: null, // String,
+    required: true,
+    default: 'dafeult artist name'
+  },
+  artistImg: {
+    type: null, // String
+    required: true
+  },
+  price: {
+    type: null, // Number
+    required: true
+  }
 })
+
+// 測試用（製作瀑布流）
+// const random = ref(Math.floor(Math.random() * 150))
+// const height = computed(() => {
+//   return 200 + random.value
+// })
 </script>
