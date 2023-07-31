ab6f4b995336fabeae63d2b49fe37b79c5b293fe
NancyHsu1994Taiwan <jjuice258tw@gmail.com>
2023-07-31 12:22:08 +0800

    feat: 平台特色手機板

--- components/home/FeatureItem.vue
+++ components/home/FeatureItem.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="mb-20 flex flex-col items-center gap-10">
     <h2>平台特色</h2>
-    <ul class="justify grid grid-cols-2">
+    <ul v-if="!isMobile" class="justify grid grid-cols-2">
       <li class="flex h-[120px] w-[416px] justify-between border-b border-[#D0D0D0] pb-5 pr-[15px]">
         <div class="flex w-full items-center justify-between">
           <div class="flex h-full flex-col justify-between">
@@ -41,7 +41,53 @@
         </div>
       </li>
     </ul>
+    <ul v-if="isMobile">
+      <li class="mb-6 flex">
+        <Icon name="ic:twotone-plagiarism" class="mr-5 h-10 w-10" />
+        <div>
+          <h4>分類篩選</h4>
+          <p class="text-secondary">透過刺青藝術的風格、元素獲取靈感</p>
+        </div>
+      </li>
+      <li class="mb-6 flex">
+        <Icon name="ic:twotone-pending-actions" class="mr-5 h-10 w-10" />
+        <div class="">
+          <h4>線上預約</h4>
+          <p class="text-secondary">說走就走，體驗一場專屬刺青冒險</p>
+        </div>
+      </li>
+      <li class="mb-6 flex">
+        <Icon name="ic:twotone-monetization-on" class="mr-5 h-10 w-10" />
+        <div class="">
+          <h4>訂金保障</h4>
+          <p class="text-secondary">先訂後刺，保障雙方買賣契約</p>
+        </div>
+      </li>
+      <li class="mb-6 flex">
+        <Icon name="ic:twotone-fact-check" class="mr-5 h-10 w-10" />
+        <div class="">
+          <h4>評價機制</h4>
+          <p class="text-secondary">安心預約經由平台認證的專業刺青師</p>
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
-<script setup></script>
+<script setup>
+import { useWindowSize } from 'vue-window-size'
+const isMobile = ref(false)
+const { width } = useWindowSize()
+watch(width, (_newValue) => {
+  if (_newValue <= 768) {
+    isMobile.value = true
+  } else {
+    isMobile.value = false
+  }
+})
+onMounted(() => {
+  if (width.value <= 768) {
+    isMobile.value = true
+  }
+})
+</script>
 <style scoped></style>
