44b7db698b6bfb9342e6f179ec6998c05e37f2cf
NancyHsu1994Taiwan <jjuice258tw@gmail.com>
2023-08-01 21:04:21 +0800

    fix: 調整樣式細節

--- layouts/account.vue
+++ layouts/account.vue
@@ -7,12 +7,12 @@
         <!-- -translate-y-[50%] -->
         <div class="left-40 flex items-center justify-center">
           <img src="/logo.png" alt="" class="mr-5 h-10 object-contain object-center" />
-          <h2 class="font-heading-typo">Inked Soul</h2>
+          <h2 class="font-heading-typo text-black">Inked Soul</h2>
         </div>
       </NuxtLink>
     </div>
 
-    <div class="col-span-7 flex h-fit flex-col items-center justify-center text-center">
+    <div class="col-span-7 flex h-full flex-col items-center justify-center text-center">
       <div class="flex w-[50%] flex-col gap-10 rounded-lg border border-black p-10">
         <slot>
           <div>登入｜註冊｜忘記密碼｜重設密碼</div>
