d7eec1dc00a284d9c2cc24727eeda4a5a20aba3e
benson <benson95217@gmail.com>
2023-08-01 13:40:10 +0800

    fix: 修正 GoTop 元件位置＆Logo 顏色

--- components/global/GoTop.vue
+++ components/global/GoTop.vue
@@ -1,6 +1,6 @@
 <template>
   <button
-    class="fixed right-[60px] top-[800px] z-10 h-11 w-11 rounded-full border-[1px] border-secondary bg-primary"
+    class="fixed bottom-[60px] right-[60px] h-11 w-11 rounded-full border border-custom bg-primary font-bold text-secondary"
     @click="goToTop()"
   >
     Top
--- components/global/NavbarItem.vue
+++ components/global/NavbarItem.vue
@@ -3,7 +3,7 @@
     class="fixed top-0 z-50 flex w-full flex-wrap items-center justify-between px-3 py-4 text-black shadow-md duration-200 ease-in-out md:min-h-[80px] md:flex-row md:px-[60px]"
     :class="[
       { 'text-white shadow-none': scrollY < 250 && route.path === '/' },
-      { 'bg-[#FFF] text-secondary': route.path !== '/' },
+      { 'bg-[#FFF] ': route.path !== '/' },
       { 'bg-[#FFF]': scrollY > 250 }
     ]"
   >
@@ -78,7 +78,7 @@
     </div>
 
     <!-- 電腦版 -->
-    <ul class="hidden items-center md:flex">
+    <ul class="hidden items-center md:flex" :class="{ 'text-secondary': route.path !== '/' }">
       <li class="h-auto w-auto">
         <NuxtLink
           to="/designs"
